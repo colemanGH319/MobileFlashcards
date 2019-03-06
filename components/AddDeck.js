4d7e00647ae981c68b8784123c5ce6244f5b8ab6
Matt Coleman <coleman.matt319@gmail.com>
2019-03-06 18:23:28 -0500

    Moved createNewDeck action creator to AddDeck component, set as handler for button

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
+import { reduxForm } from 'redux-form'
 import { connect } from 'react-redux'
 import { createNewDeck } from '../actions'
 import { Button, View, Text } from 'react-native'
 
 class AddDeck extends React.Component {
+
+  state = {
+    value: ''
+  }
+
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Add New Deck!</Text>
+
+        <Form id="newDeck">
+          <Input type="text" value={this.state.value} placeholder="Deck Name"/>
+        </Form>
+
         <Button
           title='Add Deck'
-          onPress={() => this.props.dispatch(createNewDeck('NewDeck'))}
+          onPress={() => this.props.dispatch(createNewDeck('Deck 1'))}
         />
       </View>
     );
