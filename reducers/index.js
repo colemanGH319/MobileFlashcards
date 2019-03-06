7bf0fad9b1fe9faff87a67ab79a1ed143ebd5883
Matt Coleman <coleman.matt319@gmail.com>
2019-03-06 10:09:32 -0500

    Updated switch case to handle initializeDeck. Update ADD_DECK case to properly handle new decks.

--- reducers/index.js
+++ reducers/index.js
@@ -1,17 +1,32 @@
-import { ADD_DECK, ADD_QUESTION } from '../actions'
+import { ADD_DECK, ADD_QUESTION, INITIALIZE_DECKS } from '../actions'
 
 function deck (state={}, action) {
   switch (action.type) {
     case ADD_DECK :
       return {
         ...state,
-        ...action.title
+        decks: [
+          ...state.decks,
+          {
+            id: 1,
+            name: action.name,
+            questions: []
+          }
+        ]
       }
     case ADD_QUESTION :
       return {
         ...state,
         ...action.question
       }
+
+    case INITIALIZE_DECKS :
+      return {
+        ...state,
+        'decks': []
+      }
+    default :
+      return state
   }
 }
 
