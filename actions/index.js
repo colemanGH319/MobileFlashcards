c45720aed4d0d6394376281bfca81b0188c179ee
Matt Coleman <coleman.matt319@gmail.com>
2019-03-07 20:22:19 -0500

    Added action creators for scoring.

--- actions/index.js
+++ actions/index.js
@@ -1,6 +1,9 @@
 export const ADD_DECK = 'ADD_DECK'
 export const ADD_QUESTION = 'ADD_QUESTION'
 export const INITIALIZE_DECKS = 'INITIALIZE_DECKS'
+export const INITIALIZE_SCORE = 'INITIALIZE_SCORE'
+export const CORRECT_ANSWER = 'CORRECT_ANSWER'
+export const WRONG_ANSWER = 'WRONG_ANSWER'
 
 export function createNewDeck (name) {
   return {
@@ -16,8 +19,20 @@ export function createQuestion (question) {
   }
 }
 
-export function initializeDecks () {
+export function initializeScore () {
   return {
-    type: INITIALIZE_DECKS
+    type: INITIALIZE_SCORE
+  }
+}
+
+export function scoreAnswer (isCorrect) {
+  if (isCorrect) {
+    return {
+      type: CORRECT_ANSWER
+    }
+  } else {
+    return {
+      type: WRONG_ANSWER
+    }
   }
 }
