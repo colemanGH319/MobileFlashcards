9acd38881989efddb44d569b0199536fcceec55e
Matt Coleman <coleman.matt319@gmail.com>
2019-03-07 20:30:26 -0500

    Created a random ID generator and added to createNewDeck reducer.

--- reducers/index.js
+++ reducers/index.js
@@ -1,19 +1,25 @@
-import { ADD_DECK, ADD_QUESTION, INITIALIZE_DECKS } from '../actions'
+import { combineReducers } from 'redux'
+import {
+        ADD_DECK,
+        ADD_QUESTION,
+        INITIALIZE_DECKS,
+        CORRECT_ANSWER,
+        WRONG_ANSWER,
+        INITIALIZE_SCORE
+      } from '../actions'
+import generateUID from '../utils/createId'
 
-function deck (state={}, action) {
+function decks (state={}, action) {
   switch (action.type) {
     case ADD_DECK :
+      const id = generateUID()
       return {
-        ...state,
-        decks: [
-          ...state.decks,
-          {
-            id: 1,
+          ...state,
+          [id] : {
             name: action.name,
             questions: []
           }
-        ]
-      }
+        }
     case ADD_QUESTION :
       return {
         ...state,
@@ -21,13 +27,41 @@ function deck (state={}, action) {
       }
 
     case INITIALIZE_DECKS :
+      return state
+    default :
+      return state
+  }
+}
+
+function score (state={}, action) {
+  switch(action.type) {
+    case CORRECT_ANSWER :
       return {
         ...state,
-        'decks': []
+        score: {
+          correct: state.score.correct + 1,
+          tries: state.score.tries + 1
+        }
+      }
+
+    case WRONG_ANSWER :
+      return {
+        ...state,
+        score: {
+          ...state.score,
+          tries: state.score.tries + 1
+        }
+      }
+    case INITIALIZE_SCORE :
+      return {
+        correct: 0,
+        tries: 0
       }
     default :
       return state
   }
 }
 
-export default deck
+export default combineReducers({
+  decks, score
+})
