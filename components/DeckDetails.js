769afaf7a9bd2b4f2412ea6f5a6cca1c65b4cc33
Matt Coleman <coleman.matt319@gmail.com>
2019-03-11 01:16:37 -0400

    Resolved issue with question list not updating properly.

--- components/DeckDetails.js
+++ components/DeckDetails.js
@@ -1,12 +1,63 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
+import { connect } from 'react-redux'
+import { withNavigationFocus } from 'react-navigation'
+import { createQuestion } from '../actions'
 
-export default class DeckDetails extends React.Component {
+class DeckDetails extends React.Component {
+
+  state = {
+    questions: this.props.questions,
+    lastId: 0
+  }
+
+  handleNewQuestion = (id, question) => {
+    this.props.dispatch(createQuestion(id, question))
+  }
+
+  unsubscribe = store.subscribe(() => {
+    this.state.questions = store.getState().decks[this.props.deck.deckId].questions
+    this.forceUpdate()
+  })
+
+  componentWillUnmount(){
+    this.unsubscribe()
+  }
+
   render() {
+    const { deck, questions } = this.props
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Deck Details</Text>
+        <Text>{`Title: ${deck.name}`}</Text>
+        <Text>{`Cards: ${this.state.questions.length}`}</Text>
+        <Button
+          title="Add Question"
+          onPress={() => this.props.navigation.navigate('AddCard', {
+            id: deck.deckId,
+            lastId: this.state.lastId,
+            incrementId: () => {
+              this.setState((prevState) => ({ lastId: prevState.lastId + 1 }))}
+          })}
+        />
+        <Button
+          title="Start Quiz"
+          onPress={() => this.props.navigation.navigate('Quiz', {
+            questions: this.state.questions
+          })}
+        />
       </View>
     );
   }
 }
+
+function mapStateToProps(state, { navigation }) {
+
+  const { deck } = navigation.state.params
+  const { questions } = deck
+  return {
+    deck,
+    questions
+  }
+}
+
+export default connect(mapStateToProps)(DeckDetails)
